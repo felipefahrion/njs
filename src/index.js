4e177a2b123862aded05b9544358b17baf40e20d
Felipe Fahrion <felipefahrion@POATEKMBP-Felipe-Fahrion.local>
2024-03-22 11:03:06 -0300

    extract server

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-const express = require("express");
-const { getTask, submitTask } = require("./controller.js");
+const app = require("./server");
+const { getTask, submitTask } = require("./services.js");
 const { Routes } = require("./utils/routes.js");
-const bodyParser = require('body-parser');
-
-const app = express();
-const port = 3000;
-
-app.use(bodyParser.json());
 
 app.get(Routes.GET_TASK, async (req, res) => getTask(req, res));
 app.post(Routes.SUBMIT_TASK, async (req, res) => submitTask(req, res));
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
